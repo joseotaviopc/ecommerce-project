eab3f2e813746670d5b7f466d8e5f73ab1a3b323
José Otavio Carvalho <carvalho.jose@gmail.com>
2025-02-05 11:04:15 -0300

    feat: add a log in db

--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -5,6 +5,10 @@ dotenv.config();
 
 const connectDB = async (): Promise<void> => {
   try {
+    mongoose.set('debug', (collectionName, methodName, ...methodArgs) => {
+      console.log(`[${new Date().toISOString()}] ${collectionName}.${methodName}`, methodArgs);
+    });
+
     await mongoose.connect(process.env.MONGODB_URI!);
     console.log('MongoDB connected...');
   } catch (err) {
