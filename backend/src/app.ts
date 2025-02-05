e5993b7e22a109a14ba8d05a1545f6f60420dbe9
José Otavio Carvalho <carvalho.jose@gmail.com>
2025-02-05 11:46:46 -0300

    feat: create status route & add in server

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -4,6 +4,7 @@ import connectDB from './config/db';
 import setupSwagger from './swagger';
 import productRoutes from './routes/product-routes';
 import cartRoutes from './routes/cart-routes';
+import statusRoute from './routes/status-route';
 
 const app = express();
 
@@ -12,9 +13,7 @@ connectDB();
 app.use(cors());
 app.use(express.json());
 
-app.get('/api/status', (req, res) => {
-  res.json({ message: 'API is running...' });
-});
+app.use('/api/status', statusRoute);
 app.use('/api/products', productRoutes);
 app.use('/api/cart', cartRoutes);
 
---
+++ backend/src/routes/status-route.ts
@@ -0,0 +1,27 @@
+import express from 'express';
+
+const router = express.Router();
+
+/**
+ * @swagger
+ * /api/status:
+ *   get:
+ *     summary: Return API status
+ *     tags: [Status]
+ *     description: Check if API is running.
+ *     responses:
+ *       200:
+ *         description: The API is running.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: API is running...
+ */
+router.get('/', (req, res) => {
+    res.json({ message: 'API is running...' });
+  });
+export default router;
