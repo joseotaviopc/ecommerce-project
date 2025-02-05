d061ac452eac0494731d3e9283452c060afa1de1
José Otavio Carvalho <carvalho.jose@gmail.com>
2025-02-05 10:55:00 -0300

    feat: add product routes in server

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import connectDB from './config/db';
 import setupSwagger from './swagger';
+import productRoutes from './routes/product-routes';
 
 const app = express();
 
@@ -11,8 +12,9 @@ app.use(cors());
 app.use(express.json());
 
 app.get('/api/status', (req, res) => {
-    res.json({ message: 'API is running...' });
+  res.json({ message: 'API is running...' });
 });
+app.use('/api/products', productRoutes);
 
 setupSwagger(app);
 
