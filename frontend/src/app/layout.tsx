1b38e10208bec5d7e7930d414cc8381517aa2f02
José Otavio Carvalho <carvalho.jose@gmail.com>
2025-02-06 17:24:24 -0300

    feat: add toast in app

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -2,6 +2,7 @@ import type React from "react";
 // import { Inter } from "next/font/google";
 import Providers from "./providers";
 import RootLayout from "@/components/RootLayout";
+import { Toaster } from "@/components/ui/toaster";
 import "./globals.css";
 
 // const inter = Inter({ subsets: ["latin"] });
@@ -13,6 +14,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
         <Providers>
           <RootLayout>{children}</RootLayout>
         </Providers>
+        <Toaster />
       </body>
     </html>
   );
