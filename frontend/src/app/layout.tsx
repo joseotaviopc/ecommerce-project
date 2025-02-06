e07f04a9abd61dc4465a58a45a6fd7f48bb9e4ce
José Otavio Carvalho <carvalho.jose@gmail.com>
2025-02-06 17:30:26 -0300

    fix: remove unuse & fix type error

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,11 +1,8 @@
 import type React from "react";
-// import { Inter } from "next/font/google";
 import Providers from "./providers";
 import RootLayout from "@/components/RootLayout";
 import { Toaster } from "@/components/ui/toaster";
 import "./globals.css";
-
-// const inter = Inter({ subsets: ["latin"] });
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
