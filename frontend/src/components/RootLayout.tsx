975e701068768c8cf7658f427c08712802b7bcf1
José Otavio Carvalho <carvalho.jose@gmail.com>
2025-02-06 17:31:24 -0300

    feat: add cart count in header

--- frontend/src/components/RootLayout.tsx
+++ frontend/src/components/RootLayout.tsx
@@ -1,20 +1,29 @@
-"use client"
+"use client";
 
 import type React from "react";
 import Link from "next/link";
+import { useCart } from "@/context/CartContext";
 
 export default function RootLayout(
   { children }: { children: React.ReactNode },
 ) {
+  const { cart } = useCart();
   return (
     <div className="flex min-h-screen flex-col">
       <header className="bg-background border-b">
-        <div className="container mx-auto px-4">
+        <div className="container mx-auto px-8">
           <nav className="flex h-16 items-center justify-between">
             <Link href="/" className="text-2xl font-bold">
               E-Commerce
             </Link>
-            <Link href="/cart" className="hover:underline">
+            <Link href="/cart" className="hover:underline relative">
+              {cart.length
+                ? (
+                  <span className="absolute -top-2 -right-4 rounded-full bg-white border border-gray-400 h-5 w-5 flex items-center justify-center text-red-800 font-semibold">
+                    {cart.length}
+                  </span>
+                )
+                : null}
               Cart
             </Link>
           </nav>
